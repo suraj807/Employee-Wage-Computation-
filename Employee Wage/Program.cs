c57eb3d2f50535e51261ac7d8592637e4676722b
suraj807 <surajdev805@gmail.com>
2022-05-01 12:05:43 +0530

    EmployeeAttendence

--- Employee Wage/Program.cs
+++ Employee Wage/Program.cs
@@ -10,7 +10,20 @@ namespace Employee_Wage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to EmployeeWage Computation");
+            Console.WriteLine("*****************Welcome to EmployeeWage Computation*******************\n");
+            int fulltime = 1;
+            Random random = new Random();  // random is a method 
+            int empChake = random.Next(0, 2);  // it print 0,1
+            if (empChake == fulltime)
+            {
+                Console.WriteLine ("Employee is present"); // print 1
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");    // print 0
+            }
+
+
         }
     }
 }
