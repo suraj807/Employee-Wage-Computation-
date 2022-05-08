f196dcd38201b0015f80532868fd8c23295441cd
suraj807 <surajdev805@gmail.com>
2022-05-08 09:27:15 +0530

    EmployeeWithSwitchCase

--- Employee Wage/Program.cs
+++ Employee Wage/Program.cs
@@ -11,28 +11,31 @@ namespace Employee_Wage
         static void Main(string[] args)
         {
             Console.WriteLine("*****************Welcome to EmployeeWage Computation*******************\n");
-            int isFullTime = 1;
-             int isPartTime = 2;
+            const int isFullTime = 1;
+            const int isPartTime = 2;
              int wagePerHour = 20;
             int empWorkHour = 0;
             int empDailyWage = 0;
             Random random = new Random(); // Creating object of Random class
             int check = random.Next(0, 3); // Generating random number 0  1 or 2
-             if (check == isFullTime) // Checking that employee is present for full time
+            switch(check)
             {
-                Console.WriteLine("Employee is Present for Full Time");
-                empWorkHour = 8;
+                case isFullTime:    // Checking that employee is present for full time
+                    Console.WriteLine("Employee is Present for Full Time");
+                    empWorkHour = 8;
+                    break;
+
+                    case isPartTime:   // Checking that employee is present for full time
+                    Console.WriteLine("Employee is Present for Part Time");
+                    empWorkHour = 4;
+                    break;
+
+                    default:
+                    Console.WriteLine("Employee is Absent");
+                    empWorkHour = 0;
+                    break;
             }
-            else if (check == isPartTime) //Checking that employee is present for part time
-            {
-                Console.WriteLine("Employee is Present for Part Time");
-                empWorkHour = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empWorkHour = 0;
-            }
+           
             empDailyWage = empWorkHour * wagePerHour; // Calculating Daily Wage of Employee
             Console.WriteLine($"\nEmployee Daily Wage : {empDailyWage}\n");
             Console.ReadLine();
@@ -41,3 +44,5 @@ namespace Employee_Wage
         }
     }
 }
+
+ 
