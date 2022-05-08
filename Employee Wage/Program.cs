ace7ab0da5bac4b8d65df25c43305a188aa8f384
suraj807 <surajdev805@gmail.com>
2022-05-08 18:43:10 +0530

    EmployeeMonthWage

--- Employee Wage/Program.cs
+++ Employee Wage/Program.cs
@@ -14,33 +14,40 @@ namespace Employee_Wage
             const int isFullTime = 1;
             const int isPartTime = 2;
              int wagePerHour = 20;
+            int numofworkdays = 20;
             int empWorkHour = 0;
             int empDailyWage = 0;
-            Random random = new Random(); // Creating object of Random class
-            int check = random.Next(0, 3); // Generating random number 0  1 or 2
-            switch(check)
+            int totalempwage = 0;
+            for (int day = 0; day < numofworkdays; day++) //For loop is used for calculating daily wages of Whole Month days and Total wage of Month
             {
-                case isFullTime:    // Checking that employee is present for full time
-                    Console.WriteLine("Employee is Present for Full Time");
-                    empWorkHour = 8;
-                    break;
+                Random random = new Random(); // Creating object of Random class
+                int check = random.Next(0, 3); // Generating random number 0  1 or 2
+                switch (check)
+                {
+                    case isFullTime:    // Checking that employee is present for full time
+                        Console.WriteLine("Employee is Present for Full Time");
+                        empWorkHour = 8;
+                        break;
 
                     case isPartTime:   // Checking that employee is present for full time
-                    Console.WriteLine("Employee is Present for Part Time");
-                    empWorkHour = 4;
-                    break;
+                        Console.WriteLine("Employee is Present for Part Time");
+                        empWorkHour = 4;
+                        break;
 
                     default:
-                    Console.WriteLine("Employee is Absent");
-                    empWorkHour = 0;
-                    break;
+                        Console.WriteLine("Employee is Absent");
+                        empWorkHour = 0;
+                        break;
+                }
+
+                empDailyWage = empWorkHour * wagePerHour; // Calculating Daily Wage of Employee
+                totalempwage += empDailyWage; // Adding Daily Wage to Total Wage
+                Console.WriteLine($"\nEmployee Daily Wage : {empDailyWage}\n");
+                
             }
-           
-            empDailyWage = empWorkHour * wagePerHour; // Calculating Daily Wage of Employee
-            Console.WriteLine($"\nEmployee Daily Wage : {empDailyWage}\n");
+            Console.WriteLine("total  emp Wage :" +totalempwage);
+
             Console.ReadLine();
-
-
         }
     }
 }
