9da3acb20c56432960dead079ea940186ede21be
suraj807 <surajdev805@gmail.com>
2022-05-29 22:30:09 +0530

    EmpWageForMultipleCompanies

--- Employee Wage/Program.cs
+++ Employee Wage/Program.cs
@@ -8,63 +8,60 @@ namespace Employee_Wage
 {
     internal class Program
     {
- 
         const int IS_FULL_TIME = 1;
         const int IS_PART_TIME = 2;
-        const int WAGE_PER_HOUR = 20;
-        const int MAX_WORKING_DAYS = 20;
-        const int MAX_WORK_HOURS = 100;
-        public static void ComputeWage() //Creating a class method for Wage Computation
+        public static void ComputeWage(string companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) //Creating a class method for Wage Computation with parameters
         {
             int totalMonthWage = 0, empWorkHour = 0, empDailyWage = 0, empTotalHour = 0, empTotalWorkDays = 0;
             Random random = new Random(); // Creating object of Random class
-            while (empTotalHour <= MAX_WORK_HOURS && empTotalWorkDays <= MAX_WORKING_DAYS) // Checking that Employee total work hours should be less than or equal to 100 or Employee working days should be Less or equl to 20 
+            while (empTotalHour <= maxWorkingHours && empTotalWorkDays <= maxWorkingDays) // Checking that Employee total work hours should be less than or equal to 100 or Employee working days should be Less or equl to 20 
             {
                 int check = random.Next(0, 3); // Generating random number 0 , 1 , 2
                 // Checking that employee is present for full time ,part time or not using switch
                 switch (check)
                 {
                     case IS_FULL_TIME:
-                        Console.WriteLine("Employee is Present for Full Time");
+                        // Console.WriteLine("Employee is Present for Full Time");
                         empWorkHour = 8;
                         break;
 
                     case IS_PART_TIME:
-                        Console.WriteLine("Employee is Present for Part Time");
+                        // Console.WriteLine("Employee is Present for Part Time");
                         empWorkHour = 4;
                         break;
-
                     default:
-                        Console.WriteLine("Employee is Absent");
+                        // Console.WriteLine("Employee is Absent");
                         empWorkHour = 0;
                         break;
                 }
-                empDailyWage = empWorkHour * WAGE_PER_HOUR; // Calculating Daily Wage of Employee
-                Console.WriteLine($"Employee Daily Wage for Day {empTotalWorkDays} : {empDailyWage}\n");
+                empDailyWage = empWorkHour * wagePerHour; // Calculating Daily Wage of Employee
+                                                          // Console.WriteLine($"Employee Daily Wage for Day {empTotalWorkDays} : {empDailyWage}\n");
                 totalMonthWage += empDailyWage; // Adding Daily Wage to Total Wage
                 empTotalWorkDays++;
                 empTotalHour += empWorkHour;
             }
-            if (empTotalHour > MAX_WORK_HOURS) //Checking that hours are more than 100 or not
+            if (empTotalHour > maxWorkingHours) //Checking that hours are more than 100 or not
             {
-                int a = empTotalHour - MAX_WORK_HOURS;
+                int a = empTotalHour - maxWorkingHours;
                 empTotalHour -= a;
-                int wage = a * WAGE_PER_HOUR; // Calculate exatra hours wage
+                int wage = a * wagePerHour; // Calculate exatra hours wage
                 totalMonthWage -= wage; // Minus extra hours wage from emp total wage
             }
-            if (empTotalWorkDays > MAX_WORKING_DAYS)
+            if (empTotalWorkDays > maxWorkingDays)
             {
                 empTotalWorkDays -= 1;
             }
-            Console.WriteLine($"\nEmployee total working days  : {empTotalWorkDays}");
+            Console.WriteLine("\n\nCompany Name : " + companyName);
+            Console.WriteLine($"\nEmployee total working days   : {empTotalWorkDays}");
             Console.WriteLine($"Employee total working hours  : {empTotalHour}\n");
-            Console.WriteLine($"\nEmployee Total Month Wage : {totalMonthWage}\n");
-            Console.ReadLine();
+            Console.WriteLine($"Employee Total Month Wage : {totalMonthWage}\n");
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Compuatation Program\n");
-            ComputeWage();
+            ComputeWage("Bridgelabz", 20, 20, 100);
+            ComputeWage("Infosys", 40, 20, 100);
+            Console.ReadLine();
         }
     }
 }
